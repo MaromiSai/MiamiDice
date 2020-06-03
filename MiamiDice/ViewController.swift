e604ba8006b9e9befe5dde3b0b954fe5e8d4af68
J.KEELS <maromisai@gmail.com>
2020-06-03 12:56:01 -0400

    update assets

--- MiamiDice/ViewController.swift
+++ MiamiDice/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func pressedRollButton(_ sender: Any) {
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView.image = diceArray[5]
+        
+        
+    }
+    
 }
 
